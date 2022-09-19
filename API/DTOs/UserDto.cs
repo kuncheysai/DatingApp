5f66fce636816236a0d7425f7b97668325a66041
kuncheysai <kuncheysai@gmail.com>
2022-09-19 23:59:17 +0530

    End of Section 4

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class UserDto
+    {
+        public String Username { get; set; }
+        public String Token { get; set; }
+    }
+}
